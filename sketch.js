3271e096b229828f61d6e0a18c756962d1668c8b
Cinmaya <72077672+Cinmaya@users.noreply.github.com>
2020-12-31 18:20:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint
+
+var bobDiameter;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+    
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	bobDiameter=30
+	roof = new Roof(350,100,300,20)
+	
+	bob1=new Bob(250,300,30);
+	bob2=new Bob(300,300,30);
+	bob3=new Bob(350,300,30);
+	bob4=new Bob(400,300,30);
+	bob5=new Bob(450,300,30);
+	
+
+	rope1 = new Rope(bob1.body,roof.body,-bobDiameter*2,0);  
+	rope2 = new Rope(bob2.body,roof.body,-bobDiameter*2,0);
+	rope3 = new Rope(bob3.body,roof.body,-bobDiameter*2,0);
+	rope4 = new Rope(bob4.body,roof.body,-bobDiameter*2,0);
+	rope5 = new Rope(bob5.body,roof.body,-bobDiameter*2,0);
+	
+
+
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+	
+  rectMode(CENTER);
+  background(0);
+  
+  roof.display();
+
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+
+
+ 
+}
+
+function keyPressed(){
+if(keyCode === UP_ARROW) {
+Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-730,y:0});
+
+}
+
+}
+
+
